dd775034a9ba5dd191e2792f9a9df1273ea41728
Peye <lautaro.bombace@gmail.com>
2024-04-17 16:29:14 -0300

    Prueba para ver si me acuerdo como subir un repo

---
+++ index.js
@@ -0,0 +1,27 @@
+// let num1 = 15
+
+// let num2 = 30
+
+// let suma = num1 + num2
+// console.log (suma)
+// let numeroUsuario1 = parseInt(prompt("Ingrese un numero"))
+// console.log (numeroUsuario1 + num1)
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// let numeroUsuario1 = parseInt(prompt("Ingrese un numero mayor a 10"))
+
+// if (numeroUsuario1 > 10) {
+//     console.log("El número es mayor que 10")
+// } else {
+//     console.log("El número es igual o menor que 10")
+// }
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// let edadUsuario = parseInt (prompt("Ingrese su edad"))
+// if (edadUsuario <= 21) {
+//     document.write("Eres un adolescente")
+// } else {
+//     document.write("Eres mayor")
+// }
